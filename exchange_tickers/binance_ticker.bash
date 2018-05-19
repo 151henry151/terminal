a61dc63509e19c688a3de61ad9e60aed58e77d1f
Henry Romp <151henry151@gmail.com>
2018-05-19 14:11:38 -0400

    Replace last with price

--- exchange_tickers/binance_ticker.bash
+++ exchange_tickers/binance_ticker.bash
@@ -8,32 +8,30 @@ red=$(tput setaf 1)
 green=$(tput setaf 2)
 cyan=$(tput setaf 6)
 while true; do
-
-	tput cup 0 0	
-	read price < <(curl "$gprice" 2> /dev/null | jshon -e price) 
-	cmp=${price//[\".]} 
-	if (( prev == cmp ))  
-	then printf %s "$cyan"  
-	elif (( prev < cmp ))  
-	then printf %s "$green"
-	else printf %s "$red"; 
-	fi 	
-	printf "Binance BTCUSDT:\\n"
-	printf "\$""${price//\"/}"
-	printf "\n"
-	read last < <(curl "$gethprice" 2> /dev/null | jshon -e price)
-	cmp2=${price//[\".]}
-	if (( prev2 == cmp2 ))
-	then printf %s "$cyan"
-	elif (( prev2 < cmp2 ))
-	then printf %s "$green"
-	else printf %s "$red";
-	fi
-	printf "Binance DASHETH:\\n"
-	printf "Ξ""${price//\"/}"	
-	printf "\n"
-
-	sleep 3  	
-	prev2=$cmp2
-	prev=$cmp	
+    tput cup 0 0	
+    read price < <(curl "$gprice" 2> /dev/null | jshon -e price) 
+    cmp=${price//[\".]} 
+        if (( prev == cmp ))  
+        then printf %s "$cyan"  
+        elif (( prev < cmp ))  
+        then printf %s "$green"
+        else printf %s "$red"; 
+        fi 	
+    printf "Binance BTCUSDT:\\n"
+    printf "\$""${price//\"/}"
+    printf "\n"
+    read price < <(curl "$gethprice" 2> /dev/null | jshon -e price)
+    cmp2=${price//[\".]}
+        if (( prev2 == cmp2 ))
+        then printf %s "$cyan"
+        elif (( prev2 < cmp2 ))
+        then printf %s "$green"
+        else printf %s "$red";
+        fi
+    printf "Binance DASHETH:\\n"
+    printf "Ξ""${price//\"/}"	
+    printf "\n"
+    sleep 3  	
+    prev2=$cmp2
+    prev=$cmp	
 done
