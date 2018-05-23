c696d30c51fba18fb877fa085bbda6074cd21927
Henry Romp <151henry151@gmail.com>
2018-05-22 20:44:59 -0400

    Update

--- alerter.bash
+++ alerter.bash
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 printf "\n \nThis utility will let you know when a transaction has been seen by blockexplorer.com \n \n Enter the TXID: "
 read txid
-until curl -sf "https://blockexplorer.com/api/tx/$txid" 
+until curl -sf "https://blockexplorer.com/api/tx/$txid" >/dev/null 
 do sleep 3; printf "\nNot yet done!\n"
 done 
 printf "\n\n\nDone! We see transaction $txid"
-printf "\n\n\nHere is what we see:\n\n"
 read confirmations < <(curl -s "https://blockexplorer.com/api/tx/$txid" 2> /dev/null | jshon -e confirmations)
-printf "\n\nIt looks like the transaction has $confirmations confirmations."
+printf "\n\nIt looks like the transaction has $confirmations confirmations.\n"
+
