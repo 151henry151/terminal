661ee93324432a7a5b3b12aa46882842e3249037
Henry Romp <151henry151@gmail.com>
2018-05-22 20:28:13 -0400

    Silence the curl

--- alerter.bash
+++ alerter.bash
@@ -6,6 +6,5 @@ do sleep 3; printf "\nNot yet done!\n"
 done 
 printf "\n\n\nDone! We see transaction $txid"
 printf "\n\n\nHere is what we see:\n\n"
-curl "https://blockexplorer.com/api/tx/$txid"
-read confirmations < <(curl "https://blockexplorer.com/api/tx/$txid" 2> /dev/null | jshon -e confirmations)
+read confirmations < <(curl -s "https://blockexplorer.com/api/tx/$txid" 2> /dev/null | jshon -e confirmations)
 printf "\n\nIt looks like the transaction has $confirmations confirmations."
