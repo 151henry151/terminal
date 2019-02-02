e32f77ea014319a4beba97ff832960fbd79d6224
Henry Romp <151henry151@gmail.com>
2019-02-02 12:17:05 -0500

    add waiting_for

--- alerter.bash
+++ alerter.bash
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
-printf "\n \nThis utility will let you know when a transaction has been seen by blockexplorer.com \n \n Enter the TXID: "
+printf "\n \nEnter the TXID: "
 read txid
 until curl -sf "https://blockexplorer.com/api/tx/$txid" >/dev/null 
-do sleep 3; printf "\nNot yet done!\n"
+do sleep 3; printf "\nThe transaction is not yet found!\n"
 done 
-printf "\n\n\nDone! We see transaction $txid"
+printf "\n\n\nWe see transaction $txid"
 read confirmations < <(curl -s "https://blockexplorer.com/api/tx/$txid" 2> /dev/null | jshon -e confirmations)
-printf "\n\nIt looks like the transaction has $confirmations confirmations.\n"
-
+waiting_for=0
+until (( confirmations > 0 )) 
+do
+waiting_for=$((waiting_for+1))
+sleep 3;
+printf "\n\nWaiting for $waiting_for ... It looks like the transaction has $confirmations confirmations.\n"
+done
+dialog --title "TX CONFIRMED" \
+       --begin 3 10 --msgbox "Confirmed." 5 30
